f73ada6d35c0dca7c17bd31b4abcf924244bec19
Yajatthete <73529607+Yajatthete@users.noreply.github.com>
2020-11-19 09:59:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,48 @@
-var car, wall;
-var speed, weight;
+var bullet, wall;
+var speed, weight,thickness;
 
 function setup() {
   createCanvas(1600,400);
 
-  speed = random(55, 90);
-  weight = random(400,1500);
+  speed = random(223, 321);
+  
+  weight = random(30,52);
+  
+  thickness = random(22,83);
 
-  car = createSprite(50,200,30,30);
-  wall = createSprite(1300,200,60,height/2);
-  wall.shapeColor = color(80,80,80);
+  bullet = createSprite (50,200,40,10);
+bullet.shapeColor = color(255);
+  wall = createSprite (1200,200,thickness,height/2);
+  
 
-  car.velocityX = speed;  
+  bullet.velocityX = speed;  
 
 }
 
 function draw() {
   background("black");
   
-  
-  if(wall.x-car.x < (car.width+wall.width)/2)
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2)
   {
-    car.velocityX = 0;
-    var deform = (0.5 * weight * speed * speed)/22500;
+    bullet.velocityX = 0;
+    var damage = (0.5 * weight * speed * speed)/(thickness * thickness * thickness);
   
 
-  if(deform>180){
-car.shapeColor = color(255,0,0);
+  if(damage<10){
+wall.shapeColor = color(0,255,0);
   }
 
-  if(deform>80 && deform<180){
-    car.shapeColor = color(230,230,0);
+  if(damage>10){
+    wall.shapeColor = color(255,0,0);
   }
 
-  if(deform<180){
-    car.shapeColor = color(0,255,0);
-  }
+  console.log (speed);
+  console.log (weight);
+  console.log (thickness);
+
 }
+
   drawSprites();
 }
+ 
+
